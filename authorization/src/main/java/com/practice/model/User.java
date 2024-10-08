0ec83dabc0bff6c66749d0fbd6a75a2fc7503c3a
Jang Jiwang <jiwang0917@naver.com>
2024-10-08 11:36:34 +0900

    first

---
+++ authorization/src/main/java/com/practice/model/User.java
@@ -0,0 +1,9 @@
+package com.practice.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String username;
+    private int age;
+}
---
+++ authorization/src/main/java/com/practice/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package com.practice.repository;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+    private JdbcTemplate jdbcTemplate;
+
+    public UserRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public boolean insertUser(
+
+    ) {
+        return true;
+    }
+}
