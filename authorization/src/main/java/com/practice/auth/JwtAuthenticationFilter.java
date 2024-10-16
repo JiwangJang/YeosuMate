5f9b73fe80e515f74df6a5134d43f7dd3d74e3de
Jang Jiwang <jiwang0917@naver.com>
2024-10-16 09:31:25 +0900

    JwtAuthentication Filter 작성

---
+++ authorization/src/main/java/com/practice/auth/JwtAuthenticationFilter.java
@@ -0,0 +1,22 @@
+package com.practice.auth;
+
+import com.practice.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter {
+    private final 
+}
