1e48dd608ae17f2cd1ce17e931fd940eba7db1f5
= <jiwang0917@naver.com>
2024-10-29 08:48:01 +0900

    redis study require

---
+++ backend/src/main/java/com/yeosu_mate/backend/service/RedisService.java
@@ -0,0 +1,26 @@
+package com.yeosu_mate.backend.service;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class RedisService {
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public void setData(String key, String value, Long expiredTime) {
+        redisTemplate.opsForValue().set(key, value, expiredTime, TimeUnit.MILLISECONDS);
+    }
+
+    public String getData(String key) {
+        return (String) redisTemplate.opsForValue().get(key);
+    }
+
+    public void deleteData(String key) {
+        redisTemplate.delete(key);
+    }
+}
