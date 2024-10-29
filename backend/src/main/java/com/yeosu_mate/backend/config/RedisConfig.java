8d847f8d14dfb63822672f67953958b0004bbb0d
Jang Jiwang <jiwang0917@naver.com>
2024-10-29 17:54:29 +0900

    redis hash 저장하는거 구현

--- backend/src/main/java/com/yeosu_mate/backend/config/RedisConfig.java
+++ backend/src/main/java/com/yeosu_mate/backend/config/RedisConfig.java
@@ -4,9 +4,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Configuration
 public class RedisConfig {
@@ -17,19 +18,35 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int port;
 
+    /**
+     * 레디스와 연결을 담당하는 객체
+     */
     @Bean
     RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
     }
 
+    /**
+     * 레디스와 소통하기 위한 객체
+     * 
+     * @return
+     */
     @Bean
     RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
-
-        // 모든 경우
-        redisTemplate.setDefaultSerializer(new StringRedisSerializer());
-
         return redisTemplate;
+    }
+
+    /**
+     * 레디스와 소통하기 위한 객체인데 문자전용
+     * 
+     * @return
+     */
+    @Bean
+    StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        StringRedisTemplate template = new StringRedisTemplate();
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
     }
 }
--- backend/src/main/java/com/yeosu_mate/backend/service/RedisService.java
+++ backend/src/main/java/com/yeosu_mate/backend/service/RedisService.java
@@ -1,8 +1,8 @@
 package com.yeosu_mate.backend.service;
 
-import java.util.concurrent.TimeUnit;
-
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -10,17 +10,28 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class RedisService {
-    private final RedisTemplate<String, Object> redisTemplate;
+    RedisTemplate<String, Object> redisTemplate;
+    StringRedisTemplate stringRedisTemplate;
 
-    public void setData(String key, String value, Long expiredTime) {
-        redisTemplate.opsForValue().set(key, value, expiredTime, TimeUnit.MILLISECONDS);
+    /**
+     * Redis에 문자열 키&값을 저장하는 메서드
+     * 
+     * @param key
+     * @param value
+     */
+    public void setStrin(String key, String value) {
+        ValueOperations<String, String> valueOperations = stringRedisTemplate.opsForValue();
+        valueOperations.set(key, value);
     }
 
-    public String getData(String key) {
-        return (String) redisTemplate.opsForValue().get(key);
-    }
-
-    public void deleteData(String key) {
-        redisTemplate.delete(key);
+    /**
+     * Redis에 저장돼 있는 거 꺼내오는 메서드
+     * 
+     * @param key
+     * @return value
+     */
+    public String getString(String key) {
+        ValueOperations<String, String> valueOperations = stringRedisTemplate.opsForValue();
+        return valueOperations.get(key);
     }
 }
